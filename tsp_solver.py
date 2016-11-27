15873ce5ef9b433f9039a38b728d73ec7d149b7a
Mark Steinbrick <marksteinbrick@college.harvard.edu>
2016-11-27 18:52:13 -0500

    tsp solver has randomized and greedy functions, more to come

---
+++ tsp_solver.py
@@ -0,0 +1,54 @@
+import csv
+from random import shuffle
+
+dist_matrix = []
+with open('distance_matrix.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        dist_matrix.append(row)
+
+def randomTour(bars):
+  """Function returns length of random bar tour"""
+  shuffle(bars)
+  total_distance = 0
+  prev_bar = bars.pop(0)
+  for bar in bars:
+    total_distance += int(dist_matrix[prev_bar][bar])
+    prev_bar = bar
+  return total_distance
+
+def greedyTour(bars):
+  """
+  Function returns length of greedy bar tour
+
+  Starts by finding the shortest path among all the given bars,
+  and then choses the next closest bar until all bars have been visited
+  """
+
+  #find the first and second bars greedily
+  visited_bars = set()
+  total_distance = 0
+  min_dist = float('inf')
+  for i in range(len(bars)):
+    for j in range(i + 1, len(bars)):
+      if int(dist_matrix[i][j]) < min_dist:
+        min_dist = int(dist_matrix[i][j])
+        first_bar = i
+        second_bar = j
+  total_distance += min_dist
+  visited_bars.update([first_bar, second_bar])
+  bars = [i for j,i in enumerate(bars) if i not in visited_bars]
+  prev_bar = second_bar
+
+  #from the second bar continue to pick the next clostest bar
+  while bars:
+    min_dist = float('inf') 
+    for bar in bars:
+      if int(dist_matrix[prev_bar][bar]) < min_dist:
+        min_dist = int(dist_matrix[prev_bar][bar])
+        next_bar = bar
+    total_distance += min_dist
+    visited_bars.add(prev_bar)
+    bars = [i for j,i in enumerate(bars) if i not in visited_bars]
+    prev_bar = next_bar
+  return total_distance
