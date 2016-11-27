f502040cfdc16d89b3edad877018f084005f0394
geoffstevens8 <geoff.developer@gmail.com>
2016-11-27 18:01:07 -0500

    knapsack framework

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+Notes:
+goal -> bar crawl in Boston
+    - best bar crawl
+    - most stars, cheapest cost, shortest distance
+    - consolidate stars / price -> number ('utility')
+
+Start:
+1. Pick max distance for algo
+
+1. Knapsack
+    - simulated annealing
+    - changes: remove, swap, add
+
+2. TSP
+    - takes in list of bars
+    - finds the shortest path between them all
+
+Ongoing Questions
+1. Do we want a starting place?
+2. Where to import the CSVs and convert to ints
+
+
+Interesting Things to Explore
+1. bar utility functions
+2. different TSP solvers
+3. weighting of swap, remove, add functions in simulated annealing
+
+'''
