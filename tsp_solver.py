5af0353edf40b89f100fe36f4a93099682e23313
Mark Steinbrick <marksteinbrick@college.harvard.edu>
2016-11-27 21:32:00 -0500

    2-opt added

--- tsp_solver.py
+++ tsp_solver.py
@@ -7,14 +7,20 @@ with open('distance_matrix.csv', 'rb') as f:
     for row in reader:
         dist_matrix.append(row)
 
+def getRouteDistance(bars):
+  total_distance = 0
+  prev_bar = bars[0]
+  for i in range(1, len(bars)):
+    total_distance += int(dist_matrix[prev_bar][bars[i]])
+    prev_bar = bars[i]
+  return total_distance
+
+
+
 def randomTour(bars):
   """Function returns length of random bar tour and the path of the tour"""
   shuffle(bars)
-  total_distance = 0
-  prev_bar = bars.pop(0)
-  for bar in bars:
-    total_distance += int(dist_matrix[prev_bar][bar])
-    prev_bar = bar
+  total_distance = getRouteDistance(bars)
   return (total_distance, bars)
 
 def greedyTour(bars):
@@ -53,5 +59,25 @@ def greedyTour(bars):
     prev_bar = next_bar
   return (total_distance, visited_bars)
 
-result = randomTour(range(20))
-print result
+def twoOptSwap(bars, i, k):
+  route_begin = bars[:i]
+  route_middle = bars[i:k + 1][::-1]
+  route_end = bars[k+1:]
+  return route_begin + route_middle + route_end
+
+
+def twoOptTour(bars):
+  best_distance = getRouteDistance(bars)
+  best_route = bars
+  is_swap = True
+  while is_swap:
+    is_swap = False
+    for i in range(len(bars)):
+      for k in range(i + 1, len(bars)):
+        new_route = twoOptSwap(bars, i, k)
+        new_distance = getRouteDistance(new_route)
+        if new_distance < best_distance:
+          best_distance = new_distance
+          best_route = new_route
+          is_swap = True
+  return (best_distance, best_route)
