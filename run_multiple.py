82ea4bc1ef0309a09ecfcd22d37eefbefb8315da
geoffstevens8 <geoff.developer@gmail.com>
2016-12-06 15:32:06 -0500

    updated run multiple

--- run_multiple.py
+++ run_multiple.py
@@ -8,4 +8,22 @@ import config
 import matplotlib.pyplot as plt
 import numpy as np
 
-(r, n, d, u) = knapsack.simulated_annealing_knapsack(dist, tsp)
+
+dist = 20000
+tsps = ['random', 'greedy', 'two-opt']
+
+all_tsps = []
+
+for tsp in tsps:
+    curr = []
+    for i in range(10):
+        (r, n, d, u) = knapsack.simulated_annealing_knapsack(dist, tsp)
+        curr.append(u[-1])
+    all_tsps.append(curr)
+
+plt.figure()
+random_plot, = plt.plot(all_tsps[0], label='random')
+greedy_plot, = plt.plot(all_tsps[1], label='greedy')
+twoopt_plot, = plt.plot(all_tsps[2], label='twoopt')
+plt.legend([random_plot, greedy_plot, twoopt_plot], ['Random', 'Greedy', 'Two-Opt'])
+plt.show()
